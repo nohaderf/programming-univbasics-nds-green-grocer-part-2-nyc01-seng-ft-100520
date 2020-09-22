ac7b8f819694a942fff00e93f15fe0997d318c64
Freda Hon <freda.hon@gmail.com>
2020-09-22 23:41:13 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -6,6 +6,7 @@ require 'pry'
 # REMEMBER: This method **should** update cart
 
 def apply_coupons(cart, coupons)
+  counter = 0
   coupons.each do |coupon|
     item_in_cart = find_item_by_name_in_collection(coupon[:item], cart) 
     cart_item_w_coupon = find_item_by_name_in_collection(coupon[:item] + " W/COUPON", cart)
