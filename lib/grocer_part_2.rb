26c377a1a400945812780a7dfe99a3cfd20048c7
Freda Hon <freda.hon@gmail.com>
2020-09-22 23:56:25 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -55,9 +55,9 @@ end
 
 
 
-def checkout(cart, coupon)
+def checkout(cart, coupons)
   consolidated_cart = consolidate_cart(cart)
-  couponed_cart = apply_coupons(consolidated_cart, coupon)
+  couponed_cart = apply_coupons(consolidated_cart, coupons)
   final_cart = apply_clearance(couponed_cart)
   # binding.pry
   total = 0
